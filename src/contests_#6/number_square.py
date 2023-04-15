3b26751e40e6147d9fdb439e6b5cf57149d20932
Its-yayo <a01754574@tec.mx>
2023-04-14 19:28:33 -0600

    Update test failed number_squared.py

--- src/contests_#6/number_square.py
+++ src/contests_#6/number_square.py
@@ -3,21 +3,16 @@
 """ Code to square an arg number until it reaches 65536. 
     Outputs the squared number and the number of iterations. """
 
-def solution(n: int) -> int:
-    i = 0
-
-    while n < 65536:
-        n = n ** 2
-        i += 1
-
-        if (n == 65536):
-            break
-
-    return n, i
-
 def main():
     n = int(input("Enter a number: "))
-    print(solution(n))
+    i = 0
+
+    if n >= 2 and n <= 1000000: 
+        while n < 30000:
+            n *= n
+            i += 1  
+        
+        print(n, i)
 
 if __name__ == "__main__":
     main()
