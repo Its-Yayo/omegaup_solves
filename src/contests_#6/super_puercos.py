f7761e34192ce1bc0431f3373867f79ba8c220dd
Its-Yayo <a01754574@itesm.mx>
2023-04-15 20:24:51 -0600

    Update super_puercos.py fixed

--- src/contests_#6/super_puercos.py
+++ src/contests_#6/super_puercos.py
@@ -7,3 +7,19 @@ def main():
         Test case example:
         Input -> 5 okja porky petunia babe peggy
         Output -> 3 """
+
+    n = int(input())
+    puercos = []
+
+    for i in range(0, n):
+        animals = input().strip().lower()
+        letter = animals[0]
+
+        if letter not in [p[0] for p in puercos]:
+            puercos.append(animals)
+    
+    final = len(puercos)
+    print(final)
+
+if __name__ == '__main__':
+    main()
